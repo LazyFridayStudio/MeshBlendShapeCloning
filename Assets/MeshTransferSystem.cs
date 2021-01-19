8778e0effeec00976ee3fe97c65e51eeaf471d86
Jake Gaske <Jakeco19@hotmail.com>
2021-01-19 09:50:20 +0800

    Working Version

--- Assets/MeshTransferSystem.cs
+++ Assets/MeshTransferSystem.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class MeshTransferSystem : MonoBehaviour
 {
     public SkinnedMeshRenderer meshToClone;
-    public Mesh ClonnedMeshSystem;
     public bool CloneMesh;
     public List<int> blendsToClone;
-
-    public SkinnedMeshRenderer skinnedMeshRef;
     public SkinnedMeshRenderer meshToChange;
-    
 
     // Start is called before the first frame update
     void Start()
@@ -32,25 +27,10 @@ public class MeshTransferSystem : MonoBehaviour
     void cloneMeshFunc()
     {
         Mesh meshSharedClone = meshToClone.sharedMesh;
-
-        
-        
-        Mesh Vertextpoints = new Mesh();
-        meshToClone.BakeMesh(Vertextpoints);
-        
         Mesh clonedMesh = new Mesh();
-        skinnedMeshRef.BakeMesh(clonedMesh);
+        meshToClone.BakeMesh(clonedMesh);
 
-        //meshSharedClone.vertices = clonedMesh.vertices;
-        // clonedMesh.vertices = Vertextpoints.vertices;
-       //  clonedMesh.triangles = Vertextpoints.triangles;
-       //  clonedMesh.uv = Vertextpoints.uv;
-       //  clonedMesh.normals = Vertextpoints.normals;
-       //  clonedMesh.colors = Vertextpoints.colors;
-       //  clonedMesh.tangents = Vertextpoints.tangents;
-       // clonedMesh.bindposes = meshSharedClone.bindposes;
-
-       clonedMesh.ClearBlendShapes();
+        clonedMesh.ClearBlendShapes();
 
         string blendName;
         Vector3[] deltaVertices;
@@ -63,21 +43,17 @@ public class MeshTransferSystem : MonoBehaviour
             deltaNormals = new Vector3[meshSharedClone.vertexCount];
             deltaTangets = new Vector3[meshSharedClone.vertexCount];
 
-            for (int shapeIndex = 0; shapeIndex < blendsToClone.Count; shapeIndex++)
-            {
-                blendName = meshSharedClone.GetBlendShapeName(I);
-                int frameCount = meshSharedClone.GetBlendShapeFrameCount(shapeIndex);
+            blendName = meshSharedClone.GetBlendShapeName(I);
+            int frameCount = meshSharedClone.GetBlendShapeFrameCount(I);
 
-                for (int frameIndex = 0; frameIndex < frameCount; frameIndex++)
-                {
-                    float frameWeight = meshSharedClone.GetBlendShapeFrameWeight(shapeIndex, frameIndex);
-                    meshSharedClone.GetBlendShapeFrameVertices(shapeIndex, frameIndex, deltaVertices, deltaNormals, deltaTangets);
-                    clonedMesh.AddBlendShapeFrame(blendName, frameWeight, deltaVertices, deltaNormals, deltaTangets);
-                }
+            for (int frameIndex = 0; frameIndex < frameCount; frameIndex++)
+            {
+                float frameWeight = meshSharedClone.GetBlendShapeFrameWeight(I, frameIndex);
+                meshSharedClone.GetBlendShapeFrameVertices(I, frameIndex, deltaVertices, deltaNormals, deltaTangets);
+                clonedMesh.AddBlendShapeFrame(blendName, frameWeight, deltaVertices, deltaNormals, deltaTangets);
             }
         }
 
-        ClonnedMeshSystem = clonedMesh;
-        //meshToChange.sharedMesh = ClonnedMeshSystem;
+        meshToChange.sharedMesh = clonedMesh;
     }
 }
